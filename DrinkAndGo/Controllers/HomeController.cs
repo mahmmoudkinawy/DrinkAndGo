f3e9550210a1a2721e1f8ba2723d25e22cf18399
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-03-29 23:46:08 +0200

    Update css

--- DrinkAndGo/Controllers/HomeController.cs
+++ DrinkAndGo/Controllers/HomeController.cs
@@ -2,9 +2,6 @@
 using DrinkAndGo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DrinkAndGo.Controllers
 {
